a9ce0c24ad4f91d43d6209bc23bf82cbd79b26a6
oliverweissl <o.weissl@student.vu.nl>
2023-07-04 10:55:10 +0200

    upd

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ parser.add_argument("-b", "--baseline", type=str, default=None)
 
 
 def main(filetype: str, archive: str, baseline: str):
-    dd = DataDict(archive, filetype, baseline)
-    c = CompareDict(data_dict=dd)
+    dd = DataDict(archive=archive,
+                  filetype=filetype,
+                  baseline=baseline)
+    dd.get_data()
+
+    c = CompareDict(filetype=dd.filetype,
+                    data_dict=dd.data_dict,
+                    all_frequency_values=dd.all_frequency_values)
+    c.run_comparison()
 
 
 
 if __name__ == '__main__':
-    main(filetype=parser["filetype"],
-         archive=parser["archive"],
-         baseline=parser["baseline"])
+    args = vars(parser.parse_args())
+    main(filetype=args["filetype"],
+         archive=args["archive"],
+         baseline=args["baseline"])
